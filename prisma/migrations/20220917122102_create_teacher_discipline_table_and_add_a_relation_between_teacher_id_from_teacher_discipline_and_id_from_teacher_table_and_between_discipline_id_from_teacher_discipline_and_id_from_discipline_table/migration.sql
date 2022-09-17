cbef97d88d3992f59f0c025732e5ce04e124049e
Paloma <casagrande.paloma@gmail.com>
2022-09-17 09:53:25 -0300

    [feat] creates repoprovas db by prisma

---
+++ prisma/migrations/20220917122102_create_teacher_discipline_table_and_add_a_relation_between_teacher_id_from_teacher_discipline_and_id_from_teacher_table_and_between_discipline_id_from_teacher_discipline_and_id_from_discipline_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "teacherDisciplines" (
+    "id" SERIAL NOT NULL,
+    "teacherId" INTEGER NOT NULL,
+    "disciplineId" INTEGER NOT NULL,
+
+    CONSTRAINT "teacherDisciplines_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "teacherDisciplines" ADD CONSTRAINT "teacherDisciplines_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "teachers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teacherDisciplines" ADD CONSTRAINT "teacherDisciplines_disciplineId_fkey" FOREIGN KEY ("disciplineId") REFERENCES "disciplines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
